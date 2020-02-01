eb2a6f67274381f0d34c8d069fdd7a8a167ad8fe
Heuer-Joel <heuer.joel@hotmail.com>
2020-02-01 12:49:15 +0100

    [First Commit] Interactive Germany-Map & interactive Radardiagram. No additional styling for visualizations. No algorithm for gepris-data, just pseudo-data.

---
+++ bundeslaenderData.js
@@ -0,0 +1,94 @@
+/*  Wenn Fachbereich wie "Informatik" ausgewaehlt wird,
+ *  werden die values geupdatet.
+ */
+
+var bundeslaenderObjectData = [
+    // Mecklenburg-Vorpommern
+    [
+        {   axis: "BIP", value: 0.22    },
+        {   axis: "Institute", value: 0.43    },
+        {   axis: "Projekte", value: 0.27    },
+        {   axis: "Forscher", value: 0.64    },
+        {   axis: "Spezialisierung", value: 0.06    }
+    ],
+    // Schleswig-Holstein,
+    [
+        {   axis: "BIP", value: 0.43    },
+        {   axis: "Institute", value: 0.65    },
+        {   axis: "Projekte", value: 0.34    },
+        {   axis: "Forscher", value: 0.05    },
+        {   axis: "Spezialisierung", value: 0.33    }
+    ],
+    // Niedersachsen
+    [
+        {   axis: "BIP", value: 0.22    },
+        {   axis: "Institute", value: 0.43    },
+        {   axis: "Projekte", value: 0.58    },
+        {   axis: "Forscher", value: 0.95    },
+        {   axis: "Spezialisierung", value: 0.12    }
+    ],
+    //  Bremen
+    [
+        {   axis: "BIP", value: 0.44    },
+        {   axis: "Institute", value: 0.76    },
+        {   axis: "Projekte", value: 0.44    },
+        {   axis: "Forscher", value: 0.34    },
+        {   axis: "Spezialisierung", value: 0.36    }
+    ],
+    // Sachsen-Anhalt
+    [
+        {   axis: "BIP", value: 0.97    },
+        {   axis: "Institute", value: 0.54    },
+        {   axis: "Projekte", value: 0.33    },
+        {   axis: "Forscher", value: 0.43    },
+        {   axis: "Spezialisierung", value: 0.32    }
+    ],
+    // Brandenburg
+    [
+        {   axis: "BIP", value: 0.08    },
+        {   axis: "Institute", value: 0.08    },
+        {   axis: "Projekte", value: 0.56    },
+        {   axis: "Forscher", value: 0.32    },
+        {   axis: "Spezialisierung", value: 0.65    }
+    ],
+
+/*    
+    //  Berlin
+    [
+
+    ],
+    // Nordrhein-Westfalen
+    [
+
+    ],
+    // Hessen
+    [
+
+    ],
+    //  Thüringen
+    [
+
+    ],
+    // Sachsen
+    [
+
+    ],
+    //  Rheinland-Pfalz
+    [
+
+    ],
+    // Saarland
+    [
+
+    ],
+    // Baden-Württemberg
+    [
+
+    ],
+    // Bayern
+    [
+
+    ],
+*/
+
+]
